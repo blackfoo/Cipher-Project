2e94441d2f42db35456812ef909bd784874ecf96
MItchell Greenan <mitchell.greenan@outlook.com>
2019-04-07 04:49:36 +0000

    Added some comments and more on algorithm.
    Still not working properly.

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
-#include<stdio.h>
- 
+/* Caesar Cipher project by Mitchell Greenan.
+   
+   Project will be designed to (hopefully) take in a message that is
+   input from user and then be encrypted.
+ */
+#include<stdio.h> 
 
 int main()
 {
-    char alph[26];
-    int k = 3;
-    char m = 71;
-    sprintf(alph,"GETLIT"); //storing 'GETLIT' into array.
+    char alph[100];
+    int i; // counter
+    int k = 1; // key value
+    int m = 72; // ASCII value of letter 'G'
     
-    alph = (m + k) % 26;  
-
-    printf("%s", alph);
+    sprintf(alph,"GETLIT"); // storing 'GETLIT' into array.
+                            /* with a key shift of +1, "HFUMJU" 
+                               should be displayed. */
+    for (i = 0; i < 6; i++) {
+        alph[i] = (m - 65 + k) % 26 + 65;
+    }
+    printf("%s\n", alph);
     
-   
     
-
 }
