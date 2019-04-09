88956a2206b57ada965b79faa0fe033beb02424b
MItchell Greenan <mitchell.greenan@outlook.com>
2019-04-09 01:23:53 +0000

    Fixed minor error with help from instructor.
    Text encryption is now live!

--- main.c
+++ main.c
@@ -9,16 +9,16 @@ void encryptRotationCi (void);
 
 int main()
 {
-    // create options for user to decide which task to operate.
-    char option;
+    // create task menu for user.
+    char option = 'a';
     
     printf("\nWassup'?! Select a task to operate below by entering 'a', 'b','c' or 'd' followed by the 'Enter' key.\n\n");
     printf("(a) Encrypt a word with rotation cipher!\n");
     printf("(b) Decrypt a word with rotation cypher!\n");
     printf("(c) Encrypt a word with substitution cipher!\n");
-    printf("(d) Decrypt a word with substitution cipher!\n");
+    printf("(d) Decrypt a word with substitution cipher!\n\n");
     
-    scanf("%c", &option);
+    //scanf("%c", &option);
     
     switch (option) {
         case 'a': encryptRotationCi ();
@@ -35,18 +35,19 @@ int main()
     
 void encryptRotationCi (void) {
     
-    char alph[100];
+    char alph[100] /*= "GETLIT"*/;
     int i; // counter
     int k = 1; // key value
     int m = 71; // ASCII value of letter 'G'
     
-    sprintf(alph,"GETLIT"); // storing 'GETLIT' into array.
-                            /* with a key shift of +1, "HFUMJU" 
+    sprintf(alph,"GETLIT"); /* storing 'GETLIT' into array.
+                               With a key shift of +1, "HFUMJU" 
                                should be displayed. */
     
     // creating loop to apply algorithm that encrypts the phrase.
     for (i = 0; i < 6; i++) {
-        alph[i] = (((m-65) + k) % 26) + 65; //caesar algorithm
+        m = alph[i];
+        alph[i] = (((m - 65) + k) % 26) + 65; //caesar algorithm
         printf("%c", alph[i]);
     }
     return;
