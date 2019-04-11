cebf1051fd8c95279d2d50eb256aec0ae91669e7
MItchell Greenan <mitchell.greenan@outlook.com>
2019-04-11 01:01:51 +0000

    latest update: configured rotation cipher, did my head in

--- main.c
+++ main.c
@@ -9,10 +9,12 @@ void encryptRotationCi (void);
 
 void decryptRotationCi (void);
 
+void encryptSubCi (void);
+
 int main()
 {
     // create task menu for user.
-    char option = 'a';
+    char option = 'c';
     
     printf("\nWassup'?! Select a task to operate below by entering 'a', 'b','c' or 'd' followed by the 'Enter' key.\n\n");
     printf("(a) Encrypt a word with rotation cipher!\n");
@@ -27,8 +29,8 @@ int main()
                   break;
         case 'b': decryptRotationCi ();
                   break;
-        //case 'c': encryptSubCi ();
-        //          break;
+        case 'c': encryptSubCi ();
+                  break;
         //case 'd': decryptSubCi ();
         //          break;
     }
@@ -47,9 +49,11 @@ void encryptRotationCi (void) {
     
     // creating loop to apply algorithm that encrypts the phrase.
     for (i = 0; i < alph[i]; i++) {
-        m = alph[i]; // allocates variable 'm' to first value of alpharray[] (G), then increments to following letter.
-        alph[i] = (((m - 65) + k) % 26) + 65; //caesar algorithm for encryption
-        printf("%c", alph[i]);
+        if ((alph[i] > 64) && (alph[i] < 91)) {
+            m = alph[i]; // allocates variable 'm' to first value of alpharray[] (G), then increments to following letter.
+            alph[i] = (((m - 65) + k) % 26) + 65; //caesar algorithm for encryption
+            printf("%c", alph[i]);
+        }
     }
     return;
 }
@@ -61,7 +65,7 @@ void decryptRotationCi (void) {
     int k = 1; // key value
     int m; // will be used for storing initial value of array
     
-    sprintf(alph, "HFUMJU"); /* storing 'HFUMJU' into array.
+    sprintf(alph, "HFUMJU");//"HFUMJU"); /* storing 'HFUMJU' into array.
                              //  With a key shift of -1, "GETLIT" 
                               // should be displayed. */
     
@@ -74,5 +78,33 @@ void decryptRotationCi (void) {
         }
         printf("%c", alph[i]);
     }
+    return;
+}
+
+void encryptSubCi (void) {
+    
+    char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    char subCi[100];
+    int i, j;
+    
+    sprintf(subCi, "GETLIT"); // encrypted code should read "UTZSOZ"
+    printf("%s\n", subCi);
+    
+    for (i = 0; i < oldAlph[i]; i++) {
+        printf("%c", oldAlph[i]);
+    }
+    printf("\n");
+    
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 26; j++) {
+            if (subCi[i] == oldAlph[j]) {
+                subCi[i] = newAlph[j];
+                break;
+            }
+            
+        }
+        
+    } printf("%s", subCi);
     return;
 }
