b324c0425ace0557e5e0b120a6ca2c8e424dbd31
MItchell Greenan <mitchell.greenan@outlook.com>
2019-04-11 03:52:37 +0000

    Added comments for clarity.
    Also rotation decrypt function now live! 
    
    WOO

--- main.c
+++ main.c
@@ -5,47 +5,51 @@
  */
 #include<stdio.h> 
 
-void encryptRotationCi (void);
+void encryptRotationCi (void); // function prototype for rotation cipher encryption.
 
-void decryptRotationCi (void);
+void decryptRotationCi (void); // function prototype for rotation cipher decryption.
 
-void encryptSubCi (void);
+void encryptSubCi (void); // function prototype for substitution cipher encryption.
+
+void decryptSubCi (void); // function prototype for substitution cipher decryption.
 
 int main()
 {
-    // create task menu for user.
-    char option = 'c';
+    char option = 'd'; // initialising type 'char' variable so user is able to type readable characters for menu selection.
     
+    // create task menu for user.
     printf("\nWassup'?! Select a task to operate below by entering 'a', 'b','c' or 'd' followed by the 'Enter' key.\n\n");
     printf("(a) Encrypt a word with rotation cipher!\n");
-    printf("(b) Decrypt a word with rotation cypher!\n");
+    printf("(b) Decrypt a word with rotation cypher!\n");          
     printf("(c) Encrypt a word with substitution cipher!\n");
-    printf("(d) Decrypt a word with substitution cipher!\n\n");
+    printf("(d) Decrypt a word with substitution cipher!\n\n");  // menu selection corresponds to each task. 
     
     //scanf("%c", &option);
     
     switch (option) {
-        case 'a': encryptRotationCi ();
-                  break;
+        case 'a': encryptRotationCi ();  // switch() case used for menu selection, followed 
+                  break;                 // by function prototype for each case.
         case 'b': decryptRotationCi ();
                   break;
         case 'c': encryptSubCi ();
                   break;
-        //case 'd': decryptSubCi ();
-        //          break;
+        case 'd': decryptSubCi ();
+                  break;
     }
 }
 
+// The following function will be used to encrypt using the rotation cipher.
+
 void encryptRotationCi (void) {
     
-    char alph[100] /*= "GETLIT"*/;
+    char alph[100];
     int i; // counter
     int k = 1; // key value
-    int m; // ASCII value of letter 'G'
+    int m; // will be used in loop to rotate through 'alph[]' array.
     
     sprintf(alph, "GETLIT"); /* storing 'GETLIT' into array.
-                              // With a key shift of +1, "HFUMJU" 
-                              // should be displayed. */
+                                With a key shift of +1, "HFUMJU" 
+                                should be displayed. */
     
     // creating loop to apply algorithm that encrypts the phrase.
     for (i = 0; i < alph[i]; i++) {
@@ -58,6 +62,8 @@ void encryptRotationCi (void) {
     return;
 }
 
+// The following function will be used to decrypt using the rotation cipher.
+
 void decryptRotationCi (void) {
     
     char alph[100];
@@ -65,9 +71,9 @@ void decryptRotationCi (void) {
     int k = 1; // key value
     int m; // will be used for storing initial value of array
     
-    sprintf(alph, "HFUMJU");//"HFUMJU"); /* storing 'HFUMJU' into array.
-                             //  With a key shift of -1, "GETLIT" 
-                              // should be displayed. */
+    sprintf(alph, "HFUMJU"); /* storing 'HFUMJU' into array.
+                                With a key shift of -1, "GETLIT" 
+                                should be displayed. */
     
     // creating loop to apply algorithm that encrypts the phrase.
     for (i = 0; i < alph[i]; i++) {
@@ -81,30 +87,68 @@ void decryptRotationCi (void) {
     return;
 }
 
+// The following function will be used to encrypt using the substitution cipher.
+
 void encryptSubCi (void) {
     
-    char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    char subCi[100];
-    int i, j;
+    char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array to store initial alphabet.
+    char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM"; // array to store the "new alphabet" which will be used to scramble he given word.
+    char subCi[100]; // array that will store the word to encrypt.
+    int i, j; // will be used as counter for "oldAlph[]" and "newAlph[]".
     
     sprintf(subCi, "GETLIT"); // encrypted code should read "UTZSOZ"
-    printf("%s\n", subCi);
+    printf("%s\n", subCi); // printing given word before being encrypted for comparison.
     
-    for (i = 0; i < oldAlph[i]; i++) {
-        printf("%c", oldAlph[i]);
-    }
-    printf("\n");
-    
-    for (i = 0; i < 6; i++) {
+    /* the following loop will be used to search through each array index of both
+       "oldAlph[]" and "newAlph[]". When location is found that matches the 0th 
+       index of "subCi[]" array in the "oldAlph[]" array, that letter will then be
+       swapped with the letter of the same index value in "newAlph[]". Loop should
+       continue this beahaviour until every letter in "subCi[]" has been swapped.
+       
+       e.g subCi[0] = G, this letter corresponds with 'U' in the "newAlph[]"
+       i.e G will encrypt to U and so on. */
+      
+    for (i = 0; i < subCi[i]; i++) {
         for (j = 0; j < 26; j++) {
             if (subCi[i] == oldAlph[j]) {
                 subCi[i] = newAlph[j];
                 break;
-            }
-            
+            }          
         }
-        
-    } printf("%s", subCi);
+    } 
+    printf("%s", subCi);
+    return;
+}
+
+// The following function will be used to decrypt using the substitution cipher.
+
+void decryptSubCi (void) {
+    
+    char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array to store initial alphabet.
+    char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM"; // array to store the "new alphabet" which will be used to scramble he given word.
+    char subCi[100]; // array that will store the word to encrypt.
+    int i, j; // will be used as counter for "oldAlph[]" and "newAlph[]".
+    
+    sprintf(subCi, "UTZSOZ"); // encrypted code should read "UTZSOZ"
+    printf("%s\n", subCi); // printing given word before being encrypted for comparison.
+    
+    /* the following loop will be used to search through each array index of both
+       "oldAlph[]" and "newAlph[]". When location is found that matches the 0th 
+       index of "subCi[]" array in the "oldAlph[]" array, that letter will then be
+       swapped with the letter of the same index value in "newAlph[]". Loop should
+       continue this beahaviour until every letter in "subCi[]" has been swapped.
+       
+       e.g subCi[0] = G, this letter corresponds with 'U' in the "newAlph[]"
+       i.e G will encrypt to U and so on. */
+      
+    for (i = 0; i < subCi[i]; i++) {
+        for (j = 0; j < 26; j++) {
+            if (subCi[i] == newAlph[j]) {
+                subCi[i] = oldAlph[j];
+                break;
+            }          
+        }
+    } 
+    printf("%s", subCi);
     return;
 }
