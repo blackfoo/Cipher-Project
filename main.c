1eef0a2538e49f055e415676dd8e8ac66a22656d
MItchell Greenan <mitchell.greenan@outlook.com>
2019-04-26 00:24:59 +0000

    Added loop to print SPACES

--- main.c
+++ main.c
@@ -56,33 +56,43 @@ void encryptRotationCi (void) {
     
     FILE *fp;
     char alph[100];
+    char SPACE = 32;
     int i; // counter
     int k = 1; // key value
     int m; // will be used in loop to rotate through 'alph[]' array.
     
-    fp = fopen("encrypt.txt", "r");
+    fp = fopen("encrypt.txt", "r"); // opening and reading file.
     
+    // the following loop will let us know if file does not exist.
     if (fp == NULL) {
         printf("Error: file not found.");
         return;
     }
     
+    // this loop to store message into array.
     for (i = 0; i < 100; i++) {
         fscanf(fp, "%c", &alph[i]);
     }
     
+    // this will print message before encryption.
     printf("%s\n\n", alph);
 
     // creating loop to apply algorithm that encrypts the phrase.
     for (i = 0; i < alph[i]; i++) {
-        if ((alph[i] > 64) && (alph[i] < 91)) {
+        if (alph[i] == 32) {
+            printf("%c", SPACE);
+        } else if ((alph[i] > 64) && (alph[i] < 91)) {
             m = alph[i]; // allocates variable 'm' to first value of alpharray[] (G), then increments to following letter.
             alph[i] = (((m - 65) + k) % 26) + 65; //caesar algorithm for encryption
+            printf("%c", alph[i]);
+        } else if ((alph[i] > 96) && (alph[i] < 123)) {
+            m = alph[i]; // allocates variable 'm' to first value of alpharray[] (G), then increments to following letter.
+            alph[i] = (((m - 97) + k) % 26) + 65; //caesar algorithm for encryption
             printf("%c", alph[i]);
         }
     }
     
-    fclose(fp);
+    fclose(fp); // closes the file.
     
     return;
 }
@@ -92,33 +102,46 @@ void encryptRotationCi (void) {
 void decryptRotationCi (void) {
     
     FILE *fp;
+    char SPACE = 32;
     char alph[100];
     int i; // counter
-    int k = 7; // key value
+    int k = 1; // key value
     int m; // will be used for storing initial value of array
     
-    fp = fopen("RoDecrypt.txt", "r");
+    fp = fopen("RoDecrypt.txt", "r"); // open and reads file.
     
+    // following loop implemented incase file not found.
     if (fp == NULL) {
         printf("Error: file not found.");
         return;
     }
     
-    for (i = 0; i < 100; i++) {
+    // this loop is to store message into array.
+    for (i = 0; i < 100; i++) {      
         fscanf(fp, "%c", &alph[i]);
     }
     
+    // prints message before decryption.
     printf("%s\n\n", alph);
     
     // creating loop to apply algorithm that decrypts the phrase.
     for (i = 0; i < alph[i]; i++) {
-        m = alph[i]; // allocates variable 'm' to first value of alpharray[] (H), then increments to following letter.
-        alph[i] = (((m - 65) - k) % 26) + 65; //caesar algorithm for decryption
-        if (alph[i] < 65) { // to save code from breaking (hopefully)
+        if (alph[i] == 32) {
+            printf("%c", SPACE);
+        } else if ((alph[i] > 64) && (alph[i] < 91)) {
+            m = alph[i]; // allocates variable 'm' to first value of alpharray[], then increments to following letter.
+            alph[i] = (((m - 65) - k) % 26) + 65; //caesar algorithm for decryption
+        }else if ((alph[i] > 96) && (alph[i] < 123)) {
+            m = alph[i]; // allocates variable 'm' to first value of alpharray[], then increments to following letter.
+            alph[i] = (((m - 96) - k) % 26) + 64; //caesar algorithm for decryption
+        } else if (alph[i] < 65) { // to save code from breaking (hopefully)
             alph[i] = alph[i] + 26;
         }
         printf("%c", alph[i]);
     }
+    
+    fclose(fp);
+    
     return;
 }
 
@@ -126,13 +149,25 @@ void decryptRotationCi (void) {
 
 void encryptSubCi (void) {
     
+    FILE *fp;
+    char SPACE = 32;
     char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array to store initial alphabet.
     char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM"; // array to store the "new alphabet" which will be used to scramble he given word.
     char subCi[100]; // array that will store the word to encrypt.
     int i, j; // will be used as counter for "oldAlph[]" and "newAlph[]".
     
-    sprintf(subCi, "GETLIT"); // encrypted code should read "UTZSOZ"
-    printf("%s\n", subCi); // printing given word before being encrypted for comparison.
+    fp = fopen("SubEncrypt.txt", "r"); // opens file for reading
+    
+    if (fp == NULL) {
+        printf("Error: file not found.");
+        return;
+    }
+    
+    for (i = 0; i < 100; i++) {
+        fscanf(fp, "%c", &subCi[i]);
+    }
+    
+    printf("%s\n\n", subCi); // printing given word before being encrypted for comparison.
     
     /* the following loop will be used to search through each array index of both
        "oldAlph[]" and "newAlph[]". When location is found that matches the 0th 
@@ -140,18 +175,21 @@ void encryptSubCi (void) {
        swapped with the letter of the same index value in "newAlph[]". Loop should
        continue this beahaviour until every letter in "subCi[]" has been swapped.
        
-       e.g subCi[0] = G, this letter corresponds with 'U' in the "newAlph[]"
-       i.e G will encrypt to U and so on. */
+       e.g oldAlph[0] = 'A', this letter corresponds with 'Q' in the "newAlph[]"
+       i.e A will encrypt to Q and so on. */
       
     for (i = 0; i < subCi[i]; i++) {
         for (j = 0; j < 26; j++) {
-            if (subCi[i] == oldAlph[j]) {
+             if (subCi[i] == oldAlph[j]) {
                 subCi[i] = newAlph[j];
                 break;
             }          
         }
     } 
     printf("%s", subCi);
+    
+    fclose(fp);
+    
     return;
 }
 
@@ -159,22 +197,33 @@ void encryptSubCi (void) {
 
 void decryptSubCi (void) {
     
+    FILE *fp;
     char oldAlph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array to store initial alphabet.
     char newAlph[] = "QWERTYUIOPASDFGHJKLZXCVBNM"; // array to store the "new alphabet" which will be used to scramble he given word.
-    char subCi[100]; // array that will store the word to encrypt.
+    char subCi[100]; // array that will store the word to decrypt.
     int i, j; // will be used as counter for "oldAlph[]" and "newAlph[]".
     
-    sprintf(subCi, "UTZSOZ"); // encrypted code should read "UTZSOZ"
-    printf("%s\n", subCi); // printing given word before being encrypted for comparison.
+    fp = fopen("SubDecrypt.txt", "r"); // opens file for reading
+    
+    if (fp == NULL) {
+        printf("Error: file not found.");
+        return;
+    }
+    
+    for (i = 0; i < 100; i++) {
+        fscanf(fp, "%c", &subCi[i]);
+    }
+    
+    printf("%s\n\n", subCi); // printing given word before being encrypted for comparison.
     
     /* the following loop will be used to search through each array index of both
        "oldAlph[]" and "newAlph[]". When location is found that matches the 0th 
-       index of "subCi[]" array in the "oldAlph[]" array, that letter will then be
-       swapped with the letter of the same index value in "newAlph[]". Loop should
+       index of "subCi[]" array in the "newAlph[]" array, that letter will then be
+       swapped with the letter of the same index value in "oldAlph[]". Loop should
        continue this beahaviour until every letter in "subCi[]" has been swapped.
        
-       e.g subCi[0] = G, this letter corresponds with 'U' in the "newAlph[]"
-       i.e G will encrypt to U and so on. */
+       e.g subCi[0] = Q, this letter corresponds with 'A' in the "oldAlph[]"
+       i.e 'Q' will decrypt to 'A' and so on. */
       
     for (i = 0; i < subCi[i]; i++) {
         for (j = 0; j < 26; j++) {
@@ -185,5 +234,6 @@ void decryptSubCi (void) {
         }
     } 
     printf("%s", subCi);
+    fclose(fp);
     return;
 }
